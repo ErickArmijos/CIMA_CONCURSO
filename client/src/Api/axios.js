93132b9906c1de1e7010f2d99fdaab69d92e3a30
John Ichina <john.ichina@espoch.edu.ec>
2024-06-26 23:16:09 -0500

    Allow remote

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = "http://127.0.0.1:4000/api"
+const URL = "http://192.168.100.104:4000/api"
 
 export const registerRequest = user => axios.post(URL+"/register",user);
 export const loginRequest = user => axios.post(URL+"/login",user)
